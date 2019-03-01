848720cd231942e85521de55ca5af004e396692d
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-03-01 18:18:47 +0900

    build

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,25 +1,59 @@
-/*
-* Copyright (c) 2006-2007 Erin Catto http://www.box2d.org
-*
-* This software is provided 'as-is', without any express or implied
-* warranty.  In no event will the authors be held liable for any damages
-* arising from the use of this software.
-* Permission is granted to anyone to use this software for any purpose,
-* including commercial applications, and to alter it and redistribute it
-* freely, subject to the following restrictions:
-* 1. The origin of this software must not be misrepresented; you must not
-* claim that you wrote the original software. If you use this software
-* in a product, an acknowledgment in the product documentation would be
-* appreciated but is not required.
-* 2. Altered source versions must be plainly marked as such, and must not be
-* misrepresented as being the original software.
-* 3. This notice may not be removed or altered from any source distribution.
-*/
 
 #include "Box2D/Box2D.h"
 
 #include <stdio.h>
+#include "cumino.h"
 
+class DebugDrawer : public b2Draw{
+public:
+	DebugDrawer(){
+        
+    }
+	virtual ~DebugDrawer(){}
+
+    virtual void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color){
+        print("poly %d",vertexCount);
+        for(int i=0;i<vertexCount;i++) {
+            print("  (%f,%f)",vertices[i].x,vertices[i].y);
+        }
+	}
+
+	/// Draw a solid closed polygon provided in CCW order.
+	virtual void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color){
+        print("solpoly");
+	}
+
+
+	/// Draw a circle.
+	virtual void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color){
+        print("circl");        
+	}
+	
+	/// Draw a solid circle.
+	virtual void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color){
+        print("solcircl");        
+	}
+
+	/// Draw a line segment.
+	virtual void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color){
+        print("seg");        
+	}
+
+	/// Draw a transform. Choose your own length scale.
+	/// @param xf a transform.
+	virtual void DrawTransform(const b2Transform& xf){
+        print("tr");        
+	}
+    virtual void DrawPoint(const b2Vec2& p, float32 size, const b2Color& color) {
+        print("pt");        
+    }
+
+    
+};
+
+
+
+    
 // This is a simple example of building and running a simulation
 // using Box2D. Here we create a large ground box and a small dynamic
 // box.
@@ -35,6 +69,9 @@ int main(int argc, char** argv)
 
 	// Construct a world object, which will hold and simulate the rigid bodies.
 	b2World world(gravity);
+    DebugDrawer dd;
+    dd.SetFlags(0x1f);
+    world.SetDebugDraw(&dd);
 
 	// Define the ground body.
 	b2BodyDef groundBodyDef;
@@ -96,6 +133,8 @@ int main(int argc, char** argv)
 		float32 angle = body->GetAngle();
 
 		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+
+        world.DrawDebugData();        
 	}
 
 	// When the world destructor is called, all bodies and joints are freed. This can
